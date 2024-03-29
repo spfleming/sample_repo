09b0a0189e272f88aa482cfdb3812e15037727a8
spfleming <parkerf@smu.edu>
2024-03-29 14:46:24 -0600

    figured out IQR, still need max/min

--- Python/clean_netflix.py
+++ Python/clean_netflix.py
@@ -2,3 +2,6 @@ netflix.data.clean <- netflix.data |> do_a_bunch_of_shit()
 
 
 # Ok, I got the mean and standard deviation, still need to figure out code for IQR
+
+
+netflix.data$IQR <- I_did_things()
